a578519d997944f28acb5e82a6ffbb58e61b0769
Amna Sohail <115891962+amnasohail21@users.noreply.github.com>
2025-09-01 18:53:53 +0300

    root.tsx

--- root.tsx
+++ root.tsx
@@ -9,7 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
-import {usePuterStore} from "lib/puter";
+import {usePuterStore} from "~/lib/puter";
 import {useEffect} from "react";
 
 export const links: Route.LinksFunction = () => [
@@ -27,6 +27,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   const { init } = usePuterStore();
+
   useEffect(() => {
     init()
   }, [init]);
